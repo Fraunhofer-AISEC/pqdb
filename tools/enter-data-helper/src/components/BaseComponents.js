27ffad3f33339e79e3c12db11b7b5eb07e0e75ef
Benjamin Zengin <benjamin.zengin@aisec.fraunhofer.de>
2020-04-17 13:27:01 +0200

    Fixes #57

--- tools/enter-data-helper/src/components/BaseComponents.js
+++ tools/enter-data-helper/src/components/BaseComponents.js
@@ -149,7 +149,13 @@ class JsonFormsContainer extends React.Component {
 class SelectList extends React.Component {
     constructor(props) {
         super(props);
-        this.entries = props.entries;
+        this.state = {entries: props.entries};
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.entries !== this.props.entries) {
+            this.setState({ entries: this.props.entries });
+        }
     }
 
     render() {
@@ -157,7 +163,7 @@ class SelectList extends React.Component {
             <Paper>
                 <List component="ul">
                     {
-                        this.entries.map(entry => (
+                        this.state.entries.map(entry => (
                             <ListItem button key={"item-" + entry} onClick={() => this.props.action(entry)}>
                                 <ListItemText primary={entry} />
                                 <ListItemIcon>
