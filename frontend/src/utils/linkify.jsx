78aad4d6b257a9c1e23ea7ec05d376d2ec81064d
Marian Sigler <sigler@inf.fu-berlin.de>
2021-05-05 17:10:25 +0200

    Ignore punctuation at the end of a url
    (That is, don't consider it a part of the url.)

--- frontend/src/utils/linkify.jsx
+++ frontend/src/utils/linkify.jsx
@@ -2,6 +2,6 @@ import { Link } from '@material-ui/core';
 import React from 'react';
 
 export default function linkify(s) {
-  const url = s.match(/https?:\/\/[^\s]+/g);
+  const url = s.match(/https?:\/\/[^\s]*[^-.,;()]/g);
   return url ? <Link href={url}>{s}</Link> : s;
 }
